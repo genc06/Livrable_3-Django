4c5e113cde80a5e5fb8648d6ea863d30c7619652
Genc Demi <139209233+genc06@users.noreply.github.com>
2023-12-20 16:46:34 +0100

    Migrations de ma base de donées et insertion de quelques exercices et categories

--- ExercicesMusculation/Exercices/models.py
+++ ExercicesMusculation/Exercices/models.py
@@ -3,16 +3,23 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
+
 class Categorie(models.Model):
     nom = models.CharField(max_length=25)
 
     def __str__(self):
         return self.nom
 
+
 class Exercice(models.Model):
     nom = models.CharField(max_length=100)
-    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.nom
+
+
+class CategorieExercice(models.Model):
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
+    exercice = models.ForeignKey(Exercice, on_delete=models.CASCADE)
+
