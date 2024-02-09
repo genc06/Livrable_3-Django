b2c6c1960225d21476dc107b174106d4a4317978
Genc Demi <139209233+genc06@users.noreply.github.com>
2024-02-09 11:27:33 +0100

    c

--- ExercicesMusculation/Exercices/views.py
+++ ExercicesMusculation/Exercices/views.py
@@ -1,10 +1,15 @@
 
 from django.shortcuts import render
-from .models import Exercice
-from .models import Categorie
+from .models import Exercice, Categorie
 from .models import CategorieExercice
+
+
 
 def test(request):
     exercices = Exercice.objects.all()
     return render(request, 'Liv3_base.html', {'exercices': exercices})
 
+
+def afficheCategorie(request):
+    categories = Categorie.objects.all()
+    return render(request, 'Liv3_categorie.html', {'categories': categories})
