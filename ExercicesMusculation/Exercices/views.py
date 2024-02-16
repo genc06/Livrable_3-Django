04454926073cc370b2e3ea05e863ddde0a5a4fcf
Genc Demi <139209233+genc06@users.noreply.github.com>
2024-02-16 10:24:31 +0100

    modification du CRUD

--- ExercicesMusculation/Exercices/views.py
+++ ExercicesMusculation/Exercices/views.py
@@ -1,15 +1,38 @@
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Exercice, Categorie
+from django.http import HttpResponse
 
-
-
-
-def test(request):
+def afficheExercices(request):
     exercices = Exercice.objects.all()
     return render(request, 'Liv3_base.html', {'exercices': exercices})
-
 
 def afficheCategorie(request):
     categories = Categorie.objects.all()
     return render(request, 'Liv3_categorie.html', {'categories': categories})
+
+def delete_category(request, category_id):
+    if request.method == 'POST':
+        category = Categorie.objects.get(pk=category_id)
+        category.delete()
+        return redirect('categories')
+    else:
+        return HttpResponse("Method Not Allowed", status=405)
+
+def update_category(request, category_id):
+    if request.method == 'POST':
+        updated_name = request.POST.get('updated_name')
+        category = Categorie.objects.get(pk=category_id)
+        category.nom = updated_name
+        category.save()
+        return redirect('categories')
+    else:
+        return HttpResponse("Method Not Allowed", status=405)
+
+def add_category(request):
+    if request.method == 'POST':
+        new_category_name = request.POST.get('new_category_name')
+        category = Categorie(nom=new_category_name)
+        category.save()
+        return redirect('categories')
+    else:
+        return HttpResponse("Method Not Allowed", status=405)
