7ce7d4ad9cf050a17ce1c78adc75737e92dd6cda
Genc Demi <139209233+genc06@users.noreply.github.com>
2024-02-16 10:28:47 +0100

    page Admin : inclusion des modeles

--- ExercicesMusculation/Exercices/admin.py
+++ ExercicesMusculation/Exercices/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Exercice, Categorie, CategorieExercice
 
-# Register your models here.
+admin.site.register(Exercice)
+admin.site.register(Categorie)
+admin.site.register(CategorieExercice)
+
