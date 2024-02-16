9c15c955e58884f8ceb333cfece06a750f138bd6
Genc Demi <139209233+genc06@users.noreply.github.com>
2024-02-16 11:57:26 +0100

    refactor name file

--- ExercicesMusculation/Musculation/asgi.py
+++ ExercicesMusculation/Musculation/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ExercicesMusculation project.
+ASGI config for Musculation project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ExercicesMusculation.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Musculation.settings')
 
 application = get_asgi_application()
