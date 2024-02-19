33a6901a49ffef7883c2135b08d20d374d06be3f
Genc Demi <139209233+genc06@users.noreply.github.com>
2024-02-19 16:03:25 +0100

    delete project due to inefficiency of the main framework

--- ExercicesMusculation/Exercices/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Exercice, Categorie, CategorieExercice
-
-
-admin.site.register(Exercice)
-admin.site.register(Categorie)
-admin.site.register(CategorieExercice)
-
